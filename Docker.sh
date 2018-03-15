93abe4ed6bffbf93ff2d2b66ab6219116fe61302
vasumoka <36964940+vasumoka@users.noreply.github.com>
2018-03-15 20:33:16 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,6 +1,6 @@
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-sudo systemctl status docker
+apt-get install -y docker-ce
+systemctl status docker
